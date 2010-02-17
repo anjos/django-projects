79a36976122566e3be9bf98b9a628ce357a9d268
André Anjos <rabello@pcuw32.cern.ch>
2010-02-17 14:24:45 +0100

    Major cleanup of media includes
    * Moved files around
    * Made more specific tags for different includes
    * Minimized the number of includes per template

--- djpro/templatetags/djpro_media.py
+++ djpro/templatetags/djpro_media.py
@@ -12,17 +12,17 @@ from djpro.conf import settings
 from djpro.utils import *
 
 @register.inclusion_tag('djpro/embed/media_syntax.html')
-def syntax_highlight_media(theme=settings.DJPRO_HIGHLIGHT_STYLE):
+def djpro_syntax_highlight_media(theme=settings.DJPRO_HIGHLIGHT_STYLE):
   return {'theme': theme} 
 
 @register.inclusion_tag('djpro/embed/media_bubble.html')
-def djpro_bubble_media(url=settings.MEDIA_URL): 
+def djpro_bubble_media(url=settings.settings.MEDIA_URL): 
   return {'MEDIA_URL': url}
 
 @register.inclusion_tag('djpro/embed/media_repository.html')
-def djpro_repository_media(url=settings.MEDIA_URL): 
+def djpro_repository_media(url=settings.settings.MEDIA_URL): 
   return {'MEDIA_URL': url}
 
 @register.inclusion_tag('djpro/embed/media_repository_list.html')
-def djpro_repository_list_media(url=settings.MEDIA_URL): 
+def djpro_repository_list_media(url=settings.settings.MEDIA_URL): 
   return {'MEDIA_URL': url}
